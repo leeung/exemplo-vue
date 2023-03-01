3313ec3a69edc43a767c81028952f80216757fd5
Leeung <leeungalves@gmail.com>
2023-03-01 14:30:44 -0300

    inicial4

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+//import HelloWorld from './components/HelloWorld.vue'
+//import TheWelcome from './components/TheWelcome.vue'
+
+import { defineComponent } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import Produtos from './components/Produtos.vue'
+
+import BarraLateral from './components/BarraLateral.vue';
+
+export default defineComponent({
+    name: "App",
+    components:{
+      HelloWorld,
+      BarraLateral
+    }
+});
+
+</script>
+
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <BarraLateral/>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1><img src="../assets/logo.svg" alt=""></h1>
+    </header>
+</template>
+
+<script lang="ts">
+    import { defineComponent, DefineComponent } from 'vue';
+
+    export default defineComponent({
+        name:"BarraLateral"
+    })
+</script>
+
+<style scoped>
+    header{
+        padding: 1rem;
+        background-color: red;
+        width: 100%;
+        height: 100vh;
+    }
+
+    @media only screen and (max-width: 768px){
+        header{
+            padding: 2.5 rem;
+            height: auto;
+        }
+    }
+    
+</style>
